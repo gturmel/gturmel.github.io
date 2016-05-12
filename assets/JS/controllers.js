05fbeb222a3e282c3a4a556e7384063ae02576e6
Greg Turmel <gregturmel@gmail.com>
2016-05-12 17:42:45 -0400

    add module and controller

---
+++ assets/JS/controllers.js
@@ -0,0 +1,10 @@
+portfolio.controller('PortfolioController', ['$scope', function($scope){
+
+   $scope.setTab = function(number){
+      console.log('number');
+      console.log(number);
+   };
+console.log('controller works');
+
+
+}]); //end of portfolio controller
